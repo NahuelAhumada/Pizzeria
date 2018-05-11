c98519e5ad96da5e90f1cf5ad21603b623a332b7
Yance <soy.yanceciy@gmail.com>
2018-05-11 11:39:01 -0300

    Variedad acomodo sus datos

--- src/Pizza.java
+++ src/Pizza.java
@@ -5,50 +5,48 @@
  */
 
 
-
-
 /**
  *
  * @author ITUOM
  */
 public class Pizza {
+
     //pre: clasificacion de tamaños Grande: 8 porc.+100 Extra Grande: 10 porc.+120 y Ultra Grande:12 porc.+130
     private int tamanio;
-    
     private Tipo tipo;
     private Variedad variedad;
 
     public Pizza(int tamanio, Tipo tipo, Variedad variedad) {
-        if(tamanio!=8||tamanio!=10||tamanio!=12){
+        if (tamanio != 8 || tamanio != 10 || tamanio != 12) {
             throw new Error("Tamanio invalido");
         }
+
         this.tamanio = tamanio;
         this.tipo = tipo;
         this.variedad = variedad;
-        
+
     }
-    
-    
-    public float calcularCosto(){
-        float resultado=0;
+
+    public float calcularCosto() {
+        float resultado = 0;
         float precioTamanio;
         float precioTipo;
-        if(tamanio==8){
-            precioTamanio=100f;
-        }else if(tamanio==10){
-            precioTamanio=120f;
-        }else{
-            precioTamanio=130f;
+        if (tamanio == 8) {
+            precioTamanio = 100f;
+        } else if (tamanio == 10) {
+            precioTamanio = 120f;
+        } else {
+            precioTamanio = 130f;
         }
-        
-        if(tipo==Tipo.MOLDE){
-            precioTipo=40;
-        }else if(tipo==Tipo.PIEDRA){
-            precioTipo=20;
-        }else{
-            precioTipo=30;
+
+        if (tipo == Tipo.MOLDE) {
+            precioTipo = 40;
+        } else if (tipo == Tipo.PIEDRA) {
+            precioTipo = 20;
+        } else {
+            precioTipo = 30;
         }
-        resultado=variedad.getPrecio()+precioTamanio+precioTipo;
+        resultado = variedad.getPrecio() + precioTamanio + precioTipo;
         return resultado;
     }
 }
