9e17f42397b479601a31511488ab49dd815ef8a3
Nahuel <ahumada.nahuel35149@gmail.com>
2018-05-18 10:26:03 -0300

    Cambio clase pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,3 +1,10 @@
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -23,6 +30,35 @@ public class Pedido {
     }
     public float getPrecio(){
         return pedido.calcularCosto();
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public String getHoraCreacion() {
+        return horaCreacion;
+    }
+
+    public int getTiempoDemoraMinutos() {
+        return tiempoDemoraMinutos;
+    }
+
+    public ItemPedido getPedido() {
+        return pedido;
+    }
+    public boolean pedidoAtrasado(Calendar horaActual)throws Exception{
+        Calendar cal=Calendar.getInstance();
+        String[]valores=horaCreacion.split(":");
+        int hora=Integer.valueOf(valores[0]);
+        int minutos=Integer.valueOf(valores[1]);
+        int segundos=Integer.valueOf(valores[2]);
+        cal.set(Calendar.HOUR_OF_DAY, hora);
+        cal.set(Calendar.MINUTE,minutos);
+        cal.set(Calendar.SECOND, segundos);
+        cal.add(Calendar.MINUTE, tiempoDemoraMinutos);
+        
+        return horaActual.before(cal);
     }
     
 }
